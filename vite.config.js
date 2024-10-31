f60c764f4a102b28c7949114c3bd83548bd0dbc6
zermoser <kittipoj.na@gmail.com>
2024-10-31 10:15:46 +0700

    chore update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/simple_dashboard_antd/',
+  base: '/dashboard_antd/',
   plugins: [react()],
   server: {
     port: 3003
